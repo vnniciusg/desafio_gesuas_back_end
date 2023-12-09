e3ad820b72ff22ab499b1ab300460fd9f0e9a037
Vinnicius Santos <vinnicius109@gmail.com>
2023-12-09 20:46:20 -0300

    feat : add infra layer w/ doctrine persist

---
+++ backend/src/Infrastructure/Persistence/CidadaoRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Domain\Entity\Cidadao;
+use App\Domain\Repository\CidadaoRepositoryInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+class CidadaoRepository implements CidadaoRepositoryInterface
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function salvarCidadao(Cidadao $cidadao) : bool
+    {
+        $this->entityManager->persist($cidadao);
+        $this->entityManager->flush();
+        return true;
+    }
+
+
+    public function buscarCidadaoPorNis(string $nis) : ?Cidadao
+    {
+        return $this->entityManager->getRepository(Cidadao::class)->findOneBy(['nis' => $nis]);
+    }
+
+    public function __toString()
+    {
+        return 'CidadaoRepository';
+    }
+}
