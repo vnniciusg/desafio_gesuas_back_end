49fd293932d0683bff8cc327a6a9283b34d4c4bf
Vinnicius Santos <vinnicius109@gmail.com>
2023-12-09 15:04:11 -0300

    feat : add cidadao entity with buss. rules

---
+++ backend/src/Domain/Entity/Cidadao.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Domain\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'tb_cidadao')]
+#[ORM\HasLifecycleCallbacks]
+class Cidadao 
+{
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 11 , nullable: false , unique: true)]
+    private $nis;
+    
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    
+    #[ORM\PrePersist]
+    public function prePersist()
+    {
+        $this->nis = $this->generateUniqueNis();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+   
+    public function getNome()
+    {
+        return $this->name; 
+    }
+
+    public function getNis()
+    {
+        return $this->nis;
+    }
+
+    private function generateUniqueNis(): string
+    {
+        $nisCode = str_pad(mt_rand(1, 99999999999), 11, '0', STR_PAD_LEFT);
+        return $nisCode;
+    }
+
+    public function setNome($name)
+    {
+        $this->name = $name;
+    }
+}
+
