654cd5c661ed25099b7958cbeb6a72e329fd806b
Vinnicius Santos <vinnicius109@gmail.com>
2023-12-11 23:21:05 -0300

    feat : symfony container config

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
